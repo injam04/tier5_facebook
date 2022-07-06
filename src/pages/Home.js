e98647d080692f10f0651f29ed5a8db12bd9c8de
injam04 <injamam@amarlab.com>
2022-07-06 16:10:15 +0600

    chat section done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,17 +8,17 @@ const Home = () => {
     <section className='homepage'>
       <div className='container-fluid'>
         <div className='row'>
-          <div className='col-md-4'>
+          <div className='col-md-3'>
             <div className='left-section'>
               <LeftBar />
             </div>
           </div>
-          <div className='col-md-4'>
+          <div className='col-md-6'>
             <div className='middle-section'>
               <MiddleBar />
             </div>
           </div>
-          <div className='col-md-4'>
+          <div className='col-md-3'>
             <div className='right-section'>
               <RightBar />
             </div>
